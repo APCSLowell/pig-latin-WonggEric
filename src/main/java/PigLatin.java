78317a144285783c2c7e23560105872f796a44c5
WonggEric <88013940+WonggEric@users.noreply.github.com>
2025-01-24 10:23:43 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -44,11 +44,11 @@ public class PigLatin {
     // more code should go here
     if (findFirstVowel(sWord) == -1) {
       return sWord + "ay";
-    } else if (findFirstVowel(sWord) == 1) {
+    } else if (findFirstVowel(sWord) == 0) {
       return sWord + "way";
     } else if (sWord.substring(0, 2).equals("qu")) {
       return sWord.substring(2)+"quay";
-    } else if (findFirstVowel(sWord)!=1) {
+    } else if (findFirstVowel(sWord)!=0) {
       return sWord.substring(1)+sWord.substring(0, 1)+"ay";
     } else {
       return "ERROR!";

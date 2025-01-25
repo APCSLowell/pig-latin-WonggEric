56a1afd04e059831e50d055cef511b3652e0d335
WonggEric <88013940+WonggEric@users.noreply.github.com>
2025-01-24 18:20:22 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -48,10 +48,8 @@ public class PigLatin {
       return sWord + "way";
     } else if (sWord.substring(0, 2).equals("qu")) {
       return sWord.substring(2)+"quay";
-    } else if (findFirstVowel(sWord)!=0) {
-      return sWord.substring(1)+sWord.substring(0, 1)+"ay";
     } else {
-      return "ERROR!";
+      return sWord.substring(1)+sWord.substring(0, 1)+"ay";
     }
   }
 }//end PigLatin class
